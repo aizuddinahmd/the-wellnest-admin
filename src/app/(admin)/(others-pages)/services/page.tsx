d859ac3dede0567eb4630ff78007b769dafbbdbe
Ahmad Aizuddin <aizuddinahmd0408@gmail.com>
2025-06-02 00:08:04 +0800

    fixed for deployment

--- src/app/(admin)/(others-pages)/services/page.tsx
+++ src/app/(admin)/(others-pages)/services/page.tsx
@@ -1,4 +1,3 @@
-import Calendar from "@/components/calendar/Calendar";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import { Metadata } from "next";
 import React from "react";
